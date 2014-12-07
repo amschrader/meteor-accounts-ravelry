328c65c54dfe1cd0208bb15ea0623800d9cdd324
Allyson Schrader <amschrader@gmail.com>
2014-12-06 20:14:39 -0800

    add version to the included packages

--- package.js
+++ package.js
@@ -6,13 +6,13 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
-  api.use('underscore', ['server']);  
-  api.use('accounts-base', ['client', 'server']);
+  api.use('underscore@1.0.1', ['server']);  
+  api.use('accounts-base@1.1.2', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('amschrader:ravelry', ['client', 'server']);
+  api.use('accounts-oauth@1.1.2', ['client', 'server']);
+  api.use('amschrader:ravelry@0.1.0', ['client', 'server']);
 
-  api.use('http', ['client', 'server']);
+  api.use('http@1.0.8', ['client', 'server']);
 
   api.add_files('ravelry-login-button.css', 'client');
   api.add_files('ravelry.js');
